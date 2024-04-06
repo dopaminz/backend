66518b8496a773f952e17ba02072cabd9060a9ff
shin-mallang <huipulci1@naver.com>
2024-04-06 14:15:59 +0900

    fix: 컴파일 에러 수정

--- src/main/java/org/dopaminz/controller/request/LoginRequest.java
+++ src/main/java/org/dopaminz/controller/request/LoginRequest.java
@@ -1,12 +1,7 @@
 package org.dopaminz.controller.request;
 
-import org.dopaminz.entity.Member;
-
 public record LoginRequest(
         String username,
         String password
 ) {
-    public Member toMember() {
-        return new Member(username, password);
-    }
 }
